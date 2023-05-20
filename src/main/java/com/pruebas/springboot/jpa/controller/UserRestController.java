947b2afce980f024136ee2ea613b958107acc545
ctobonpe <ctobonpe@gmail.com>
2023-05-20 07:04:19 -0500

    finalizado spring boot basico

--- src/main/java/com/pruebas/springboot/jpa/controller/UserRestController.java
+++ src/main/java/com/pruebas/springboot/jpa/controller/UserRestController.java
@@ -2,6 +2,7 @@ package com.pruebas.springboot.jpa.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pruebas.springboot.jpa.caseuse.CreateUser;
@@ -19,6 +21,7 @@ import com.pruebas.springboot.jpa.caseuse.DeleteUser;
 import com.pruebas.springboot.jpa.caseuse.GetUser;
 import com.pruebas.springboot.jpa.caseuse.UpdateUser;
 import com.pruebas.springboot.jpa.entity.User;
+import com.pruebas.springboot.jpa.repository.UserRepository;
 
 @RestController
 @RequestMapping("api/users")
@@ -28,13 +31,15 @@ public class UserRestController {
 	private CreateUser createUser;
 	private DeleteUser deleteUser;
 	private UpdateUser updateUser;
+	private UserRepository userRepository;
 	
 	
-	public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser,UpdateUser updateUser) {
+	public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser,UpdateUser updateUser, UserRepository userRepository) {
 		this.getUser = getUser;
 		this.createUser = createUser;
 		this.deleteUser = deleteUser;
 		this.updateUser = updateUser;
+		this.userRepository = userRepository;
 	}
 	
 	@GetMapping("/")
@@ -59,6 +64,12 @@ public class UserRestController {
 	@PutMapping("/{id}")
 	ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
 		return new ResponseEntity<User>(updateUser.replace(newUser,id),HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/pageable")
+	List<User> getuserPageable(@RequestParam int page, @RequestParam int size){
+		return userRepository.findAll(PageRequest.of(page, size)).getContent();
 	}
 	
 	
